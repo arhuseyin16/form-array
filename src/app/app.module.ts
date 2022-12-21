3647305270e631d89817fb72fbd60ca6c477b5aa
huseyin.ar <huseyin.ar@net-bt.com.tr>
2022-12-21 17:44:24 +0300

    form array example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormArrayComponent } from './form-array/form-array.component';
+import {RouterModule, Routes} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
+
+ const routes: Routes = [
+   {
+     path: 'formArray',
+     component: FormArrayComponent
+   },
+   { path: '**', redirectTo: 'formArray' }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormArrayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form-array/form-array.component.ts
+++ src/app/form-array/form-array.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-form-array',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-array.component.css']
 })
 export class FormArrayComponent implements OnInit {
+  bankForm = this.fb.group({
+    cards: this.fb.array([this.cardItem()])
+  });
 
-  constructor() { }
+  month = [1,2,3,4,5,6,7,8,9,10,11,12];
+  year = [2022, 2023, 2024, 2025, 2026, 2027, 2028]
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  cardItem(): FormGroup {
+    return this.fb.group({
+      name: [''],
+      cardNumber: [''],
+      month: [''],
+      year: [''],
+      cvv: ['']
+    });
+  }
+
+  get createdCards() {
+    return this.bankForm?.get('cards') as FormArray;
+  }
+
+  addCards() {
+    this.createdCards.push(this.cardItem());
+  }
+
+  delete(index: number) {
+    if (index !== 0) {
+      return this.createdCards.removeAt(index);
+    }
+  }
+
+  save() {
+    console.log(this.bankForm);
+  }
 }
